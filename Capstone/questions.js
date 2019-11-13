87fe668ce173567cde3b881f28b3d895377fdede
Sameera Shereen <sshereen@uncc.edu>
2019-11-13 11:52:01 -0500

    Created Assessment quiz

---
+++ Capstone/questions.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        "question": "Science",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Working with people",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Exercising",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Expressing Creativity",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Politics",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Public Speaking",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Anatomy and Physiology",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Playing instruments",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Entrepreneurship",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+    {
+        "question": "Technology",
+        "answer1": "Interested",
+        "answer1Total": "3",
+        "answer2": "Neutral",
+        "answer2Total": "2",
+        "answer3": "Not Interested",
+        "answer3Total": "1"
+    },
+]
